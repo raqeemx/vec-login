869612bf018c2c4aff7ab558dc641399dc257f1f
raqeemx <raqeemx.ai@gmail.com>
2025-12-23 18:16:02 +0300

    Update Firebase storage bucket domain

--- firebase-config.js
+++ firebase-config.js
@@ -14,7 +14,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyA7B27LIH9FPZAsMKr5lvm2AIa3ZkZNOUo",
   authDomain: "vec-login-58dea.firebaseapp.com",
   projectId: "vec-login-58dea",
-  storageBucket: "vec-login-58dea.appspot.com",
+  storageBucket: "vec-login-58dea.firebasestorage.app",
   messagingSenderId: "651347381212",
   appId: "1:651347381212:web:dc7eba2bbf69532e13dd19",
   measurementId: "G-P9Q2KBSNQH"
